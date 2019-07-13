e4912b15a8db9138f163227d1841d6c0cb0f8e3b
motek <moteko.0608.mrchildren@gmail.com>
2019-07-13 15:00:57 +0900

    initial commit

---
+++ src/Welcom4.java
@@ -0,0 +1,8 @@
+public class Welcom4 {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.printf("%s\n%s\n",
+                "Welcom to", "Java Programing!");
+    }
+}
